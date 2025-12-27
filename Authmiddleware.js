d29dc3068ecefa787158638cb5cc46a707f65196
Nikita-Bari <132455296+Nikita-Bari@users.noreply.github.com>
2025-12-28 00:56:58 +0530

    Add files via upload

---
+++ Authmiddleware.js
@@ -0,0 +1,17 @@
+// middleware/authMiddleware.js
+const jwt = require("jsonwebtoken");
+
+function auth(req, res, next) {
+  const token = req.header("Authorization");
+  if (!token) return res.status(401).json({ message: "No token, access denied" });
+
+  try {
+    const verified = jwt.verify(token, "secret_key");
+    req.user = verified;
+    next();
+  } catch (err) {
+    res.status(400).json({ message: "Invalid token" });
+  }
+}
+
+module.exports = auth;
